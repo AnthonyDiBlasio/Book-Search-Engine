25fbf1625e80537334bda6d0bb94b0946e3d4c17
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2022-08-10 10:45:27 -0400

    changing server.js to add an await

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient} from '@apollo/client';
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
 
 const client = new ApolloClient({
 	request: operation => {
@@ -24,9 +25,7 @@ function App() {
 	return (
 		<ApolloProvider client={client}>
 			<Router>
-				<>
-					<Navbar />
-					<Switch>
+				
 						<Route exact path="/" component={SearchBooks} />
 						<Route
 							exact
@@ -40,8 +39,7 @@ function App() {
 								</h1>
 							)}
 						/>
-					</Switch>
-				</>
+			
 			</Router>
 		</ApolloProvider>
 	);
--- server/server.js
+++ server/server.js
@@ -1,44 +1,39 @@
-const express = require('express');
+const express =require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
 const path = require('path');
-const db = require('./config/connection');
-
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	context: authMiddleware,
+    typeDefs,
+    resolvers,
+    context: authMiddleware
 });
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-		// log where we can go to test our GQL API
-		console.log(
-			`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-		);
-	});
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
 
-db.on('error', err => {
-	console.error('MongoDB connection error: ', err);
-});
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer(typeDefs, resolvers);
